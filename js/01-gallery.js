bd8340199f9df7b9f79ecf22bb8c38e1354944a1
PavloHolotyuk <pavloholotuk@gmail.com>
2022-11-11 12:37:48 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallaryElements = document.querySelector(".gallery");
+
+const gallery = galleryItems
+  .map(
+    (img) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${img.original}"><img class="gallery__image" src="${img.preview}" data-source="${img.original}" alt="${img.description}"/></a></div>`
+  )
+  .join("");
+
+gallaryElements.innerHTML = gallery;
+
+gallaryElements.addEventListener("click", openImg);
+
+let instance;
+
+function openImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName === "DIV") {
+    return;
+  }
+  instance = basicLightbox.create(`
+    <div class="modal">
+        <img src=${event.target.dataset.source} width="80%" style="margin: auto; display: block"> 
+    </div>
+`);
+  instance.show();
+  document.addEventListener("keydown", closeModal);
+}
+
+function closeModal(event) {
+  if (event.key === "Escape") {
+    instance.close();
+    document.removeEventListener("keydown", closeModal);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallaryElements = document.querySelector(".gallery");
+
+const gallery = galleryItems
+  .map(
+    (img) =>
+      `<a class="gallery__item" href="${img.original}"><img class="gallery__image" src="${img.preview}" data-source="${img.original}" alt="${img.description}"/></a>`
+  )
+  .join("");
+
+gallaryElements.innerHTML = gallery;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
